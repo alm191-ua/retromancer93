dbdd3b8ecff0f8920336bc16e03ea80731d77d63
Akira <alm191@alu.ua.es>
2023-10-30 17:18:14 +0100

    restar 3 puntos por enemigo y eliminar calderos de nivel 5

--- src/man/levels.s
+++ src/man/levels.s
@@ -34,7 +34,7 @@ lvl3_enemies:
 lvl4_enemies:
     .dw tmpl_enemy_void, tmpl_enemy_p, tmpl_enemy_o, tmpl_caldero, enemies_end
 lvl5_enemies:
-    .dw tmpl_enemy_o, tmpl_enemy_p, tmpl_enemy_void, tmpl_caldero, enemies_end
+    .dw tmpl_enemy_o, tmpl_enemy_p, tmpl_enemy_void, enemies_end
 
 unlocked_levels:
     .db 0x01 ;; only level one unlocked by default
